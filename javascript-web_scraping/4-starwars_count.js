8be2b761e6aac782d5aa462f40b56e090c3b86ee
Martin <martinleiro9@gmail.com>
2023-09-21 08:49:21 -0700

    fix

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -7,17 +7,16 @@ const url = process.argv[2];
 request(url, (err, data, body) => {
   if (err) {
     console.log(err);
-  } else {
-    let counter = 0;
-    const films = JSON.parse(body).results;
+  }
 
-    for (let result = 0; result < films.length; result++) {
-      const characters = films[result].characters;
+  let counter = 0;
+  const films = JSON.parse(body).results;
 
-      for (let j = 0; j < characters.length; j++) {
-        if (characters[j] === 'https://swapi-api.hbtn.io/api/people/18/' || characters[j] === 'http://swapi-api.hbtn.io/api/people/18/') {
-          counter += 1;
-        }
+  for (let result = 0; result < films.length; result++) {
+    const characters = films[result].characters;
+    for (let j = 0; j < characters.length; j++) {
+      if (characters[j] === 'https://swapi-api.hbtn.io/api/people/18/' || characters[j] === 'http://swapi-api.hbtn.io/api/people/18/') {
+        counter += 1;
       }
     }
     console.log(counter);

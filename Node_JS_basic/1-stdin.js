e9041303a67e50d8ce28de86fb7d0d00d12fc2d2
martin <martinleiro9@gmail.com>
2023-10-15 07:44:39 -0700

    fix stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,8 +3,8 @@ console.log('Welcome to Holberton School, what is your name?');
 // await input
 process.stdin.resume();
 
-process.stdin.on('readable', () => {
-  const name = process.stdin.read();
+process.stdin.on('data', (data) => {
+  const name = data.toString();
 
   process.stdout.write(`Your name is: ${name}`);
   process.stdout.write('This important software is now closing\n');

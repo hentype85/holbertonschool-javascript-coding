9b758f1cf5bdf3936db0c046b5884d31922eec33
martin <martinleiro9@gmail.com>
2023-10-15 07:50:23 -0700

    fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -7,6 +7,10 @@ process.stdin.on('data', (data) => {
   const name = data.toString();
 
   process.stdout.write(`Your name is: ${name}`);
-  process.stdout.write('This important software is now closing\n');
+
+  if(!process.stdin.isTTY) {
+    process.stdout.write('This important software is now closing\n');
+  }
+
   process.exit();
 });
